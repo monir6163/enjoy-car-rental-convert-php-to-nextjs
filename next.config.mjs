fc1f97bede88d2736d21f6c80d793010cd177103
netstermonir <monirhossain6163@gmail.com>
2024-07-25 02:08:57 +0600

    image

--- next.config.mjs
+++ next.config.mjs
@@ -8,34 +8,6 @@ const nextConfig = {
       },
     ],
   },
-
-  //security headers
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; img-src *; script-src 'self'; style-src 'self';",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
