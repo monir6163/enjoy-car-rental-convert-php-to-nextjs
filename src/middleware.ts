a5b36a86158689763ad67c46dc8637ad2e6ab9d7
netstermonir <monirhossain6163@gmail.com>
2024-08-18 20:00:59 +0600

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-import { cookies } from "next/headers";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
@@ -7,8 +6,8 @@ export async function middleware(request: NextRequest) {
 
   const isPublicPath = path === "/login" || path === "/register";
 
-  const cookieStore = cookies();
-  const token = cookieStore.get("accessToken")?.value;
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmI4OWQ3MjIxMjUxZmZlYzk0NDYzZjkiLCJlbWFpbCI6ImtyYWZzYW5hOTFAZ21haWwuY29tIiwicm9sZSI6ImFkbWluIiwiYXZhdGFyIjp7InB1YmxpY19pZCI6ImVjb20vdXNlci96emZhY3o0eWRwN2RzcXlrYmZvayIsInVybCI6Imh0dHBzOi8vcmVzLmNsb3VkaW5hcnkuY29tL2RkeHFsanJpdy9pbWFnZS91cGxvYWQvdjE3MjMzNzQ5NjIvZWNvbS91c2VyL3p6ZmFjejR5ZHA3ZHNxeWtiZm9rLmpwZyJ9LCJmdWxsbmFtZSI6IkFkbWluIiwiaWF0IjoxNzIzOTg0MTY0LCJleHAiOjE3MjM5ODc3NjR9.PFYFG_3n9TaY8f_QBWWB4yspjSqeRqxgmYrfefw3YKU";
 
   if (isPublicPath && token) {
     return NextResponse.redirect(
