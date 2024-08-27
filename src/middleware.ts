f829327106696e4e8d337218149cc234edff1538
netstermonir <monirhossain6163@gmail.com>
2024-08-27 09:05:42 +0600

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,6 @@ import { CustomUser } from "./lib/auth";
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const token = await getToken({ req: request });
-  //   * Get user from token
   const user: CustomUser | null = token?.user as CustomUser;
 
   // * Protected routes for user
@@ -14,47 +13,39 @@ export async function middleware(request: NextRequest) {
   // * Protected routes for admin
   const adminProtectedRoutes = ["/admin/dashboard"];
 
+  // * If no token or user exists and user/admin tries to access protected routes
   if (
-    token == null &&
-    user == null &&
+    !token &&
     (userProtectedRoutes.includes(pathname) ||
       adminProtectedRoutes.includes(pathname))
   ) {
+    const redirectUrl = new URL("/login", request.url);
+    redirectUrl.searchParams.set(
+      "error",
+      "Please login first to access this route"
+    );
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  // * If user tries to access admin routes
+  if (adminProtectedRoutes.includes(pathname) && user?.role === "user") {
     return NextResponse.redirect(
-      new URL(
-        "/login?error=Please login first to access this route",
-        request.url
-      )
+      new URL("/login?error=You do not have access to this route.", request.url)
     );
   }
 
-  // * if user try to access admin routes
-  if (adminProtectedRoutes.includes(pathname) && user.role === "user") {
-    return NextResponse.redirect(
-      new URL(
-        "/login?error=Please login first to access this route.",
-        request.url
-      )
-    );
-  }
-
-  //   * If Admin try to access user routes
-  if (userProtectedRoutes.includes(pathname) && user.role === "admin") {
-    return NextResponse.redirect(
-      new URL(
-        "/login?error=Please login first to access this route.",
-        request.url
-      )
-    );
-  }
-
-  //  * If user is already logged in and try to access login or register page
-  if (pathname == "/login" || pathname == "/register") {
-    if (token && user?.role === "admin") {
-      return NextResponse.redirect(new URL("/admin/dashboard", request.url));
-    } else if (token && user?.role === "user") {
-      return NextResponse.redirect(new URL("/", request.url));
+  // * If user or admin tries to access login or register page while logged in
+  if (pathname === "/login" || pathname === "/register") {
+    if (token) {
+      if (user?.role === "admin") {
+        return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+      } else if (user?.role === "user") {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
     }
     return NextResponse.next();
   }
+
+  // * If no special condition applies, proceed to the next handler
+  return NextResponse.next();
 }
