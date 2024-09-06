779ed7a33d80459de81bb6fe494e5180fa0187ad
netstermonir <monirhossain6163@gmail.com>
2024-09-07 01:06:33 +0600

    rent car page

--- src/app/(withLayout)/rent-car/page.tsx
+++ src/app/(withLayout)/rent-car/page.tsx
@@ -1,9 +1,11 @@
+import RentCar from "@/app/components/rentCar/RentCar";
 import Breadcrumb from "@/app/components/shared/Breadcrumb";
 
 export default function RenterCar() {
   return (
     <>
       <Breadcrumb label="Rent a Car" />
+      <RentCar />
     </>
   );
 }
