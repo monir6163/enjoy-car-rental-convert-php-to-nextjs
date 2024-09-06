9f1f783d4e8353e3fe2116f6f34305628cef73a7
netstermonir <monirhossain6163@gmail.com>
2024-09-07 01:12:23 +0600

    fix

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 import LoginApp from "@/app/components/auth/Login";
 
-export default async function Login() {
+export default function Login() {
   return (
     <div className="py-32  lg:px-10  min-h-[calc(100vh-90px)]">
       <LoginApp />
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,6 +1,6 @@
 import RegisterApp from "@/app/components/auth/Register";
 
-export default async function Register() {
+export default function Register() {
   return (
     <div className="py-32  lg:px-10  min-h-[calc(100vh-90px)]">
       <RegisterApp />
