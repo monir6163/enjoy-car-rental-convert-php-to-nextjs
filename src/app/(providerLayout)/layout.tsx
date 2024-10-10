fa2520926e8862332c82714eedf7662ae6925ef5
netstermonir <monirhossain6163@gmail.com>
2024-10-10 19:37:40 +0600

    provider-panel

--- src/app/(providerLayout)/layout.tsx
+++ src/app/(providerLayout)/layout.tsx
@@ -1,8 +1,5 @@
 import "@mantine/core/styles.css";
 import "react-toastify/dist/ReactToastify.css";
-import Footer from "../components/footer/Footer";
-import Mobile from "../components/navbar/Mobile";
-import Navbar from "../components/navbar/Navbar";
 import "../globals.css";
 
 export default function ProviderLayout({
@@ -10,12 +7,5 @@ export default function ProviderLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Navbar />
-      <Mobile />
-      {children}
-      <Footer />
-    </>
-  );
+  return <>{children}</>;
 }
