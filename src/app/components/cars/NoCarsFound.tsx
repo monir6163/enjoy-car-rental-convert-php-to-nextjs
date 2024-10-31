4697c03aa0c5ca747a3b0be9564fc193855c35d7
monir6163 <monirhossain6163@gmail.com>
2024-10-31 19:31:43 +0600

    not found car page

--- src/app/components/cars/NoCarsFound.tsx
+++ src/app/components/cars/NoCarsFound.tsx
@@ -1,16 +1,20 @@
-import { Card, Flex, Text, Title } from "@mantine/core";
+import { Card, Text, Title } from "@mantine/core";
 import { FaBoxOpen } from "react-icons/fa";
 
 export const NoCarsFound = () => {
   return (
-    <Flex justify="center" align="center" direction="column" gap={16}>
-      <Card withBorder style={{ backgroundColor: "transparent" }}>
-        <Text className="text-default">
-          <FaBoxOpen size="4rem" />
-        </Text>
-        <Title className="text-muted">No Cars Found</Title>
-        <Text c="gray.6">Sorry! No cars found for your search</Text>
-      </Card>
-    </Flex>
+    <Card
+      shadow="xs"
+      padding="lg"
+      radius="xl"
+      style={{ textAlign: "center" }}
+      className="flex flex-col items-center justify-center"
+    >
+      <Text className="text-default">
+        <FaBoxOpen size="4rem" />
+      </Text>
+      <Title className="text-muted">No Cars Found</Title>
+      <Text c="gray.6">Sorry! No cars found for your search</Text>
+    </Card>
   );
 };
